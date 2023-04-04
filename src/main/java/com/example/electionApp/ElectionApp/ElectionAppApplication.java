6e6cdf95b018e64c534be7023131a7edf454769d
Simex <osimiloluwa9@example.com>
2023-04-04 16:38:24 +0100

    commit

---
+++ src/main/java/com/example/electionApp/ElectionApp/ElectionAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.electionApp.ElectionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/electionApp/ElectionApp/ElectionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.electionApp.ElectionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
